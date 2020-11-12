2c438d1211061e56bad0866f7f300776cc023e25
Mik Dadayan <dadayan.mikayel@gmail.com>
2020-11-12 11:57:36 -0800

    Added functionality to add item into item list to UI ul list

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const ItemCtrl = (function () {
 
   const data = {
     items: [
-      { id: 0, name: "Steak Dinner", price: 25 },
-      { id: 1, name: "Gas", price: 40 },
-      { id: 2, name: "Mark Store", price: 45 },
+      // { id: 0, name: "Steak Dinner", price: 25 },
+      // { id: 1, name: "Gas", price: 40 },
+      // { id: 2, name: "Mark Store", price: 45 },
     ],
     currentItem: null,
     totalSpends: 0,
@@ -23,6 +23,18 @@ const ItemCtrl = (function () {
     getItems: function () {
       return data.items;
     },
+    addItem: function (name, price) {
+      let id;
+      // Create id for new item(last items added id + 1 or 0 if items list is empty)
+      if (data.items.length > 0) {
+        id = data.items[data.items.length - 1].id + 1;
+      } else {
+        id = 0;
+      }
+      const newItem = new Item(id, name, price);
+      data.items.push(newItem);
+      return newItem;
+    },
     logData: function () {
       return data;
     },
@@ -32,12 +44,14 @@ const ItemCtrl = (function () {
 // UI Controller
 const UICtrl = (function () {
   const UISelectors = {
-    itemList: "#item-list"
-  }
+    itemList: "#item-list",
+    addBtn: ".add-btn",
+    itemName: "#item-name",
+    itemPrice: "#item-price",
+  };
   // Public Methods
   return {
     populateToList: function (items) {
-      console.log("Helllooooo")
       let innerUL = "";
       items.forEach((item) => {
         innerUL += `<li class="collection-item" id="item-${item.id}">
@@ -49,26 +63,97 @@ const UICtrl = (function () {
       });
       document.querySelector(UISelectors.itemList).innerHTML = innerUL;
     },
-    getSelectors: function(){
+    getItemInputs: function () {
+      return {
+        name: document.querySelector(UISelectors.itemName),
+        price: document.querySelector(UISelectors.itemPrice),
+      };
+    },
+    getSelectors: function () {
       return UISelectors;
-    }
+    },
+    addItemToList: function (item) {
+      document.querySelector(UISelectors.itemList).style.display = "block"
+      const li = document.createElement("li");
+      li.className = "collection-item";
+      li.id = `item-${item.id}`;
+      li.innerHTML = `<strong>${item.name}: </strong> <em>${item.price} dollar</em>
+        <a href="#" class="secondary-content">
+          <i class="fa fa-pencil"></i>
+        </a>
+      `;
+      // Insert Item
+      document
+        .querySelector(UISelectors.itemList)
+        .insertAdjacentElement("beforeend", li);
+    },
+    clearInputs: function () {
+      document.querySelector(UISelectors.itemName).value = "";
+      document.querySelector(UISelectors.itemPrice).value = "";
+    },
+    hideList: function () {
+      document.querySelector(UISelectors.itemList).style.display = "none";
+    },
   };
 })();
 
 // App Controller
 const App = (function (ItemCtrl, UICtrl) {
   // load event listeners
-  const loadEventListeners = function(){
+  const loadEventListeners = function () {
     // Get UI selectors
     const UISelectors = UICtrl.getSelectors();
-  }
+
+    // Add item event
+    document
+      .querySelector(UISelectors.addBtn)
+      .addEventListener("click", itemAddSubmit);
+  };
+  // console.log(itemAddSubmit)
+
+  // Add item submit
+  const itemAddSubmit = function (e) {
+    // Get form from UI Controller
+    e.preventDefault();
+    const inputs = UICtrl.getItemInputs();
+    // Validation for inputs values
+    if (
+      inputs.name.value !== "" &&
+      typeof Number(inputs.price.value) === "number" &&
+      inputs.price.value > 0
+    ) {
+      // Add inputes values to data list and populate items to UI
+      const newItem = ItemCtrl.addItem(
+        inputs.name.value,
+        Number(inputs.price.value)
+      );
+
+      // Add new Item to our ul list in UI
+      UICtrl.addItemToList(newItem);
+
+      // Clear input fields after adding new item
+      UICtrl.clearInputs();
+    }
+  };
+
+  //Public Methods
   return {
     init: function () {
+      // Fetch items from data sturcture
       const items = ItemCtrl.getItems();
-      UICtrl.populateToList(items);
+      // check if items list is empty
+      if (items.length === 0) {
+        // Hide ul items 
+        UICtrl.hideList();
+      } else {
+        // Populate list with items
+        UICtrl.populateToList(items);
+      }
+
+      // Load event listeners
+      loadEventListeners();
     },
   };
 })(ItemCtrl, UICtrl);
 
-
-App.init()
+App.init();
